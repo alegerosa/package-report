5573266b5d6bfee0992706b0489deb6c9789cf09
Ale Gerosa <alegerosa@gmail.com>
2020-01-27 16:47:40 -0800

    Add write_packages script and output

---
+++ 01_write-installed-packages.R
@@ -0,0 +1,16 @@
+#' ---
+#' output: github_document
+#' ---
+
+library(tidyverse)
+library(here)
+
+##' Create a data frame of your installed packages
+packages <- as_tibble(installed.packages()) %>% 
+  select(Package,
+         LibPath,
+         Version,
+         Priority,
+         Built)
+
+write_csv(packages, here("data", "installed_packages.csv"))
